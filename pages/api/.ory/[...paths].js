2de664af0c9d26a6fa102ddf2ace7be4af7ef174
dbfreelance <darielbatistafreelance@gmail.com>
2022-09-16 16:23:25 -0400

    initial commit

---
+++ pages/api/.ory/[...paths].js
@@ -0,0 +1,12 @@
+import { config, createApiHandler } from '@ory/integrations/next-edge'
+
+// We need to export the config.
+export { config }
+
+// And create the Ory Cloud API "bridge".
+export default createApiHandler({
+  fallbackToPlayground: true,
+  // Because vercel.app is a public suffix and setting cookies for
+  // vercel.app is not possible.
+  dontUseTldForCookieDomain: true
+})
